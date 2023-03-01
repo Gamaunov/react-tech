4c0e5e0ebfea6b632ac00eb4a20a5820d3b09841
Gamaunov <gamaunov1911@gmail.com>
2023-03-01 19:57:31 +0300

    wrote user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import "./App.css";
+import User from "./components/user/User";
 
 function App() {
+  const users = [
+    { name: "pedro", age: 21 },
+    { name: "ja", age: 22 },
+    { name: "rwfr", age: 231 },
+  ];
   return (
     <div className="App">
-
+      {users.map((user, key) => {
+        return <User key={key} name={user.name} age={user.age} />;
+      })}
     </div>
   );
 }
+
+
 
 export default App;
