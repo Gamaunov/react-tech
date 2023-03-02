5926f4e33cf641c2a938555aab2f8bf5c0160f38
Gamaunov <gamaunov1911@gmail.com>
2023-03-02 17:48:33 +0300

    set value

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [showText, setShowText] = useState('teal');
+  const [count, setCount] = useState(0);
 
+  const increase = () => {
+    setCount(count + 1)
+  }
+  const decrease = () => {
+    setCount(count - 1)
+  }
+  const setZero = () => {
+    setCount(0)
+  }
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setShowText(showText === 'teal' ? 'red' :  'teal')
-        }}
-      >
-        show/hide
-      </button>
-       <h1 style={{color: showText}}>Hi my ame id</h1>
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
+      <button onClick={setZero}>set to zero</button>
+      {count}
     </div>
   );
 }
