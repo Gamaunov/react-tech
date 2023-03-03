8077acabe7936c42c0deab9244509b4cde071fa4
Gamaunov <gamaunov1911@gmail.com>
2023-03-03 09:56:34 +0300

    axios cat facts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-import { useState } from "react";
+import  Axios  from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Task from "./components/task/Task";
+
+
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
+const [catFact, setCatFact] = useState('')
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
+const fetchCatFact = () => {
+  Axios.get('https://catfact.ninja/fact')
+  .then(res => {
+    setCatFact(res.data.fact)
+  })
+}
 
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-    };
-    setTodoList([...todoList, task]);
-  };
+useEffect(() => {
+  fetchCatFact()
+}, [])
 
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
+
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>new task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task, key) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              deleteTask={deleteTask}
-              id={task.id}
-              key={key}
-            />
-          );
-        })}
-      </div>
+      <button 
+        onClick={fetchCatFact}
+      >
+        generate cat fact
+      </button>
+      <p>{catFact}</p>
     </div>
   );
 }
--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,7 +1,12 @@
 const Task = (props) => {
   return (
     <div className="list__inner">
-      <h1>{props.taskName}</h1>
+      <h1
+        style={{ textDecoration: props.completed ? "none" : "line-through" }}
+      >
+        {props.taskName}
+      </h1>
+      <button onClick={() => props.completeTask(props.id)}>complete</button>
       <button onClick={() => props.deleteTask(props.id)}>X</button>
     </div>
   );
